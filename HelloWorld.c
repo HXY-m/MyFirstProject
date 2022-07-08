e179746cfafbd0b1287a7ad14ffeed3937303f10
HXY <2195682742@qq.com>
2022-07-08 16:19:35 +0800

    修改后

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-int main(){
-
-
+//修改代码
+int main(void){
 	printf("Hello World!!!\n");
 	printf("My name is Hxy.\n");
 	printf("Nice to meet you.\n");
 	printf("Nice to meet you too.");
-	
-
 	return 0;
 }
